71440e9749eaf284a1962679ea3135747d06567b
Wilmar Andres Rojas Villafañe <106172929+wirovi07@users.noreply.github.com>
2024-07-26 16:21:19 -0500

    Registrar Ventas y Detalles de Venta

--- app/Http/Controllers/SaleController.php
+++ app/Http/Controllers/SaleController.php
@@ -6,6 +6,7 @@ use App\Models\Sales;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use App\Helpers\Codification;
+use App\Models\Saledetails;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -49,33 +50,46 @@ class SaleController extends Controller
 
     public function store(Request $request)
     {
+      
         $request->validate([
-            'total' => 'required|string',
-            'company_id' => 'required|int',
-            'employee_id' => 'required|int',
-            'customer_id' => 'required|array',
-            'customer_id.id' => 'required|int'
+            'total' => 'required',
+            'customer_id' => 'required',
+            'products' => 'required|array',
+            'products.*.product_id' => 'required|int',
+            'products.*.amount' => 'required|numeric',
+            'products.*.unit_price' => 'required|string',
+            'products.*.subtotal' => 'required|numeric'
         ]);
     
         $customer_id = $request->input('customer_id');
     
-        if (!isset($customer_id['id'])) {
-            return response()->json(['error' => 'Invalid customer_id format'], 400);
-        }
-    
-        Log::info('Customer ID: ' . $customer_id['id']);
-    
         try {
+            DB::beginTransaction();
             $sales = new Sales();
             $sales->date = now(); 
             $sales->total = $request->total;
             $sales->company_id = $request->company_id;
             $sales->employee_id = $request->employee_id;
-            $sales->customer_id = $customer_id['id']; 
+            $sales->customer_id = $request->customer_id; 
             $sales->save();
+
+            $sale_id = $sales->id;
+
+            foreach ($request->products as $product) {
+                $saledetail = new Saledetails();
+                $saledetail->sale_id = $sale_id;
+                $saledetail->product_id = $product['product_id'];
+                $saledetail->amount = $product['amount'];
+                $saledetail->unit_price = $product['unit_price'];
+                $saledetail->subtotal = $product['subtotal'];
+                $saledetail->save();
+            }
+
+            DB::commit();
     
             return response()->json(['message' => 'Sales created successfully']);
         } catch (QueryException $e) {
+            DB::rollBack();
             return response()->json(['message' => 'Error creating sales: ' . $e->getMessage()], 500);
         }
     }
